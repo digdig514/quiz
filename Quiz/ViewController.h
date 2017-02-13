b4c867476d9f4fde60735b9c989ebdf897680668
Diego Souza Sampaio <digdig@MacBook-Air-de-Diego.local>
2017-02-13 15:16:50 -0500

    Initial Commit

---
+++ Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Diego Souza Sampaio on 17-02-13.
+//  Copyright © 2017 Diego Souza Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Quiz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Diego Souza Sampaio on 17-02-13.
+//  Copyright © 2017 Diego Souza Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
