539cbfb08e78d24f7f92f87e991045328a6532f8
Diego Souza Sampaio <digdig@MacBook-Air-de-Diego.local>
2017-02-15 13:28:54 -0500

    Quiz

--- Quiz/ViewController.h
+++ Quiz/ViewController.h
@@ -4,12 +4,21 @@
 //
 //  Created by Diego Souza Sampaio on 17-02-13.
 //  Copyright © 2017 Diego Souza Sampaio. All rights reserved.
-//
+// all the variable will be here
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController  {
 
+NSArray *questionsArray;
+    //array declaration
+NSArray *answersArray;
+    int curQuestion;
+    int totalQuestion;
+   NSString *currentAnswer;
+}
+@property (weak, nonatomic) IBOutlet UILabel *LabelQuestion;
+@property (weak, nonatomic) IBOutlet UILabel *LabelAnswer;
 
 @end
 
--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -14,9 +14,46 @@
 
 @implementation ViewController
 
+-(void)HideLabel{
+  _LabelQuestion.text = @"";
+  _LabelAnswer.text = @"???";
+  currentAnswer= @"";
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //questionsArray = [[NSArray alloc]init];
+    [self HideLabel];
+    curQuestion =0;
+    questionsArray = @[@"My name ?", @"My age?"];
+    answersArray = @[@"Diego", @"28"];
+    totalQuestion = sizeof(questionsArray)/sizeof(int);
+    
+    
+    
+
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)QuestionOnclick:(id)sender {
+
+_LabelQuestion.text = questionsArray[curQuestion];
+    [self setAnswer];
+    curQuestion ++;
+    if (curQuestion==totalQuestion)
+    {
+        curQuestion=0;
+    }
+    
+}
+
+-(void)setAnswer {
+   currentAnswer = answersArray[curQuestion];
+    
+}
+
+- (IBAction)ResponseOnclick:(id)sender {
+    _LabelAnswer.text = currentAnswer;
 }
 
 
